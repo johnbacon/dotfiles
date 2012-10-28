427b3308cea762ef0ff7eba2b2a5d191b41ceb46
John Bacon <jpbacon@gmail.com>
2012-10-27 20:40:18 -0400

    Add parsing for user agents to the alias

--- oh-my-zsh/custom/plugins/bacon/bacon.plugin.zsh
+++ oh-my-zsh/custom/plugins/bacon/bacon.plugin.zsh
@@ -4,6 +4,8 @@ unsetopt correct_all
 # a few aliases I like
 alias gs='git status'
 alias gd='git diff'
+alias goaccess-all='sudo goaccess -f /var/log/cherokee-redstorm.com.log -a -s -e 12.220.82.229'
+alias goaccess-export='sudo goaccess -f /var/log/cherokee-redstorm.com.log -a -e 12.220.82.229 > report.html'
 
 # add plugin's bin directory to path
 export PATH="$(dirname $0)/bin:$PATH"
